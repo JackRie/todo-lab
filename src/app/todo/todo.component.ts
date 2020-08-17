51d51db69daf67616a7aefc93d08fb044fc2d47d
JackRie <riemersmajackie@gmail.com>
2020-08-17 19:05:47 -0400

    added functionality to the form

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
-
+import { NgForm } from '@angular/forms';
+import { formatCurrency } from '@angular/common';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -26,7 +27,42 @@ export class TodoComponent implements OnInit {
     },
   ];
 
+  filterTerm: string;
+
   constructor() {}
+
+  addTask(form: NgForm) {
+    let newTodo: Todo = {
+      task: form.value.add,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    let todo: Todo = this.todos[index];
+    todo.completed = true;
+  }
+
+  setFilterTerm(form: NgForm) {
+    // console.log(form);
+    this.filterTerm = form.value.filterTerm.trim().toLowerCase();
+  }
+
+  filter() {
+    if (!this.filterTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((todo) => {
+        let currentTodo = todo.task.toLowerCase().trim();
+        return currentTodo.includes(this.filterTerm);
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
